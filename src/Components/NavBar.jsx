295f38b7cd2af49a7ac1e1249f9e1dd45e97089b
TDMeyers <meyers.dylan@outlook.com>
2023-07-23 16:38:11 -0600

    Initialize, first commit

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/nav.css";
+
+function NavBar() {
+  const navigate = useNavigate();
+  return (
+    <div className="NavBarContainer">
+      <ol className="NavBar">
+        <li onClick={() => navigate("/")}>Home</li>
+        <li onClick={() => navigate("/about")}>About</li>
+        <li onClick={() => navigate("/stock")}>Dashboard</li>
+        <li onClick={() => navigate("/follow")}>Following</li>
+      </ol>
+    </div>
+  );
+}
+
+export default NavBar;

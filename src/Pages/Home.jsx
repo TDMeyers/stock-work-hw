4d75fd7d6c299896460cf077948ece9428942bc6
TDMeyers <meyers.dylan@outlook.com>
2023-07-23 16:41:30 -0600

    added home page and style

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/Home.css";
+function Home() {
+  return (
+    <div className="HomePage">
+      <h1>HomePage</h1>
+    </div>
+  );
+}
+
+export default Home;

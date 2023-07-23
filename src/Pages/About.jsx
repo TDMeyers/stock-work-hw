f9c0078286e8af9df3ee1f2e14db642471460096
TDMeyers <meyers.dylan@outlook.com>
2023-07-23 16:40:16 -0600

    Added about page, styles adjusted

---
+++ src/Pages/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/About.css";
+function About() {
+  return (
+    <div className="AboutPage">
+      <h1>AboutPage</h1>
+    </div>
+  );
+}
+
+export default About;
